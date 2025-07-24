255d4e45a04afd5597f03b8c3bc8f15292acdde9
olman26 <omartinez@loto.hn>
2025-07-24 15:34:07 -0600

    cambios footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,59 @@
+<div class="footer">
+  <div class="footer-left">
+    <img src="Abrazo BI Color.svg" alt="Logo" class="footer-logo">
+    <p class="footer-title">¡Te cambia la vida!</p>
+  </div>
+
+  <div class="footer-right">
+    <div class="footer-columns">
+      <div class="footer-column">
+        <h3>Juegos</h3>
+        <p>Bingo con Todo</p>
+        <p>Diaria</p>
+        <p>Juga tres</p>
+        <p>Súper premio</p>
+        <p>Instacash</p>
+        <p>Premia2</p>
+        <p>Multi - x</p>
+        <p>Apostemos</p>
+        <p>Online Gaming</p>
+        <p>Ganagol</p>
+      </div>
+
+      <div class="footer-column">
+        <h3>Nosotros</h3>
+        <p>Juego Responsable</p>
+        <p>RSE</p>
+        <p>Aplica con nosotros</p>
+        <p>Quiero ser vendedor</p>
+        <p>LotoCentros</p>
+        <p>Mapa puntos de venta</p>
+      </div>
+
+      <div class="footer-column">
+        <h3>Secciones</h3>
+        <p>Promociones</p>
+        <p>Noticias</p>
+        <p>Resultados</p>
+        <p>SMS</p>
+        <p>Tengo</p>
+        <p>Contáctanos</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="social-icons">
+    <img src="tik-tok.png" alt="TikTok">
+    <img src="facebook.png" alt="Facebook">
+    <img src="instagram.png" alt="Instagram">
+    <img src="youtube.png" alt="YouTube">
+    <img src="linkedin.png" alt="LinkedIn">
+  </div>
+</div>
+
+</div>
+
+
+ 
+</body>
 </html>
