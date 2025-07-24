2c841e73b2810f573bfeeed253d43b5f5e643376
olman26 <omartinez@loto.hn>
2025-07-24 15:16:56 -0600

    footer

--- includes/body.php
+++ includes/body.php
@@ -94,42 +94,42 @@
     <p class="footer-title">¡Te cambia la vida!</p>
   </div>
 
-  <div class="footer-columns">
+  <div class="footer-right">
+    <div class="footer-columns">
+      <div class="footer-column">
+        <h3>Juegos</h3>
+        <p>Bingo con Todo</p>
+        <p>Diaria</p>
+        <p>Juga tres</p>
+        <p>Súper premio</p>
+        <p>Instacash</p>
+        <p>Premia2</p>
+        <p>Multi - x</p>
+        <p>Apostemos</p>
+        <p>Online Gaming</p>
+        <p>Ganagol</p>
+      </div>
 
-    <div class="footer-column">
-      <h3>Juegos</h3>
-      <p>Bingo con Todo</p>
-      <p>Diaria</p>
-      <p>Juga tres</p>
-      <p>Súper premio</p>
-      <p>Instacash</p>
-      <p>Premia2</p>
-      <p>Multi - x</p>
-      <p>Apostemos</p>
-      <p>Online Gaming</p>
-      <p>Ganagol</p>
+      <div class="footer-column">
+        <h3>Nosotros</h3>
+        <p>Juego Responsable</p>
+        <p>RSE</p>
+        <p>Aplica con nosotros</p>
+        <p>Quiero ser vendedor</p>
+        <p>LotoCentros</p>
+        <p>Mapa puntos de venta</p>
+      </div>
+
+      <div class="footer-column">
+        <h3>Secciones</h3>
+        <p>Promociones</p>
+        <p>Noticias</p>
+        <p>Resultados</p>
+        <p>SMS</p>
+        <p>Tengo</p>
+        <p>Contáctanos</p>
+      </div>
     </div>
-
-    <div class="footer-column">
-      <h3>Nosotros</h3>
-      <p>Juego Responsable</p>
-      <p>RSE</p>
-      <p>Aplica con nosotros</p>
-      <p>Quiero ser vendedor</p>
-      <p>LotoCentros</p>
-      <p>Mapa puntos de venta</p>
-    </div>
-
-    <div class="footer-column">
-      <h3>Secciones</h3>
-      <p>Promociones</p>
-      <p>Noticias</p>
-      <p>Resultados</p>
-      <p>SMS</p>
-      <p>Tengo</p>
-      <p>Contáctanos</p>
-    </div>
-
   </div>
 
   <div class="social-icons">
@@ -139,6 +139,8 @@
     <img src="youtube.png" alt="YouTube">
     <img src="linkedin.png" alt="LinkedIn">
   </div>
+</div>
+
 </div>
 
 
