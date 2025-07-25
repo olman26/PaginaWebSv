bed79623cf2fc2570410ec58990e4d8003e34869
olman26 <omartinez@loto.hn>
2025-07-25 11:04:21 -0600

    COMMIT

--- includes/body.php
+++ includes/body.php
@@ -64,7 +64,7 @@
   <div class="youtube-content">
     <!-- Columna izquierda con video -->
     <div class="youtube-video">
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed?v=1qsx5zpIp7w" 
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/1qsx5zpIp7w?start=14" 
         title="YouTube video player" frameborder="0" allowfullscreen></iframe>
     </div>
 
