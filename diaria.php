61f5d9ce06a03b223e52664d33cb98b8cb31af6e
olman26 <omartinez@loto.hn>
2025-09-03 15:07:00 -0600

    COMMIT

--- diaria.php
+++ diaria.php
@@ -15,33 +15,9 @@
   </div>
 
     <img src="modelo.png" alt="Conductora">
+    
 </div>
-  <!-- Contenido de la página -->
-  <section class="diaria-ganador">
-    <h2>Último número ganador:</h2>
-    <div class="diaria-numeros">
-      <span>09</span>
-      <span>09</span>
-      <span>02</span>
-    </div>
-    <p class="proximo-sorteo">Próximo sorteo en vivo: 01h 55m : 35s</p>
-  </section>
 
-  <section class="diaria-anteriores">
-    <h3>Resultados anteriores</h3>
-    <input type="date" />
-    <div class="diaria-resultados">
-      <p>Sorteo 142: 09 - 13 - 02</p>
-      <p>Sorteo 141: 07 - 14 - 03</p>
-    </div>
-  </section>
-
-  <section class="diaria-como-jugar">
-    <h3>Cómo jugar y ganar</h3>
-    <p>Selecciona tu número favorito del 00 al 99.  
-    Si aciertas, ¡ganas premios instantáneos!</p>
-    <button class="btn-reglamento">Leer el reglamento</button>
-  </section>
 </main>
 
 
