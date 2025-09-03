a48b9af9995ef649c962ae525010aa82b08eda95
olman26 <omartinez@loto.hn>
2025-09-03 11:14:11 -0600

    COMMIT

--- includes/footer.php
+++ includes/footer.php
@@ -35,19 +35,19 @@
         <p>Contáctanos</p>
       </div>
     </div>
+  </div>
 
-    <!-- NUEVO: Logos en la parte inferior derecha -->
-    <div class="footer-logos">
-      <img src="Logo ESR.png" alt="Logo ESR" class="footer-extra-logo">
-      <img src="LOTO_21.png" alt="Logo Loto 21" class="footer-extra-logo">
-    </div>
+  <!-- Logos totalmente a la derecha -->
+  <div class="footer-logos">
+    <img src="Logo ESR.png" alt="Logo ESR" class="footer-extra-logo">
+    <img src="LOTO_21.png" alt="Logo Loto 21" class="footer-extra-logo">
   </div>
 
   <div class="social-icons">
     <img src="tik-tok.svg" alt="TikTok">
     <img src="instagram.svg" alt="Instagram">
     <img src="facebook.svg" alt="Facebook">
-    <img src="facebook.png" alt="Facebook">
+    <img src="Youtube.svg" alt="Youtube">
     <img src="linkedin.svg" alt="LinkedIn">
   </div>
 </div>
