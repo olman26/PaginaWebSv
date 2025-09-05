4d5f4911a4eeed2d62e9eb05516e06de1c793cf7
olman26 <omartinez@loto.hn>
2025-09-05 13:58:30 -0600

    commit

--- includes/header.php
+++ includes/header.php
@@ -20,22 +20,27 @@
     
         <!-- Cuadro naranja con logo, navegación y botón -->
         <div class="main-header">
-          <div class="logo">
-            <img src="Abrazo BI Color 1.svg" alt="Logo">
-          </div>
-          <nav class="nav-menu">
-      <a href="index.php?pag=diaria">JUEGOS</a>
-      <a href="https://loto.sv/puntos-de-venta/">RESULTADOS</a>
-      <a href="https://loto.sv/puntos-de-venta/">NOTICIAS</a>
-      <a href="https://loto.sv/puntos-de-venta/">PUNTO DE VENTA</a>
-      
-          </nav>
+  <div class="logo">
+    <img src="Abrazo BI Color 1.svg" alt="Logo">
+  </div>
 
-        <div class="play-button">
-     <a href="https://juega.loto.sv/websales/?pk_campaign=WS_SITE_BOTON_WEBSALES" target="_blank">
-     <img src="boton-jugar-en-linea.png" alt="Jugar en línea">
-     </a>
-        </div>
+  <input type="checkbox" id="menu-toggle" />
+  <label for="menu-toggle" class="hamburger">&#9776;</label>
+
+  <nav class="nav-menu">
+    <a href="index.php?pag=diaria">JUEGOS</a>
+    <a href="https://loto.sv/puntos-de-venta/">RESULTADOS</a>
+    <a href="https://loto.sv/puntos-de-venta/">NOTICIAS</a>
+    <a href="https://loto.sv/puntos-de-venta/">PUNTO DE VENTA</a>
+  </nav>
+
+  <div class="play-button">
+    <a href="https://juega.loto.sv/websales/?pk_campaign=WS_SITE_BOTON_WEBSALES" target="_blank">
+      <img src="boton-jugar-en-linea.png" alt="Jugar en línea">
+    </a>
+  </div>
+</div>
+
 
 
   </header>
