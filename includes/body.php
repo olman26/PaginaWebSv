7a5363cb4df004abf1be84438540cf3558f930d8
olman26 <omartinez@loto.hn>
2025-09-10 18:10:13 -0600

    commit

--- includes/body.php
+++ includes/body.php
@@ -226,7 +226,7 @@ window.addEventListener('scroll', function() {
     
     // Desplaza 4 cm (≈151px) hacia arriba al hacer scroll
     if (window.scrollY > 0) {
-        box.style.transform = 'translateY(-70px)';
+        box.style.transform = 'translateY(-80px)';
     } else {
         box.style.transform = 'translateY(0)';
     }
